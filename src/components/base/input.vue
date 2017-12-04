3f92f9f77cc0fb3a27ce4a0c863a303dba3f3250
shiro <sunny@yui.org.cn>
2017-12-04 18:00:47 +0800

    add input skin single

--- src/components/base/input.vue
+++ src/components/base/input.vue
@@ -1,5 +1,5 @@
 <template>
-  <label class="input-wrapper">
+  <label class="input-wrapper" :class="type">
     <span v-html="label" class="label" @click="click"></span>
     <input type="text" v-model="field" :placeholder="placeholder">
   </label>
@@ -10,7 +10,15 @@
     data() {
       return {};
     },
-    props: ['label', 'value', 'placeholder'],
+    props: {
+      label: String,
+      value: [String, Number],
+      placeholder: String,
+      type: {
+        type: String,
+        default: 'base'
+      }
+    },
     methods: {
       click() {
         this.$emit('click');
@@ -31,36 +39,58 @@
 
 <style lang="scss">
   .input-wrapper {
-    display: inline-table;
     width: 100%;
     font-size: .12rem;
-    > * {
-      display: table-cell;
-    }
-    .label {
-      width: 1px;
-      height: 2.5em;
-      padding: 0 .1rem;
-      border-radius: 4px 0 0 4px;
-      border: 1px solid #bbb;
-      border-right: 0;
-      line-height: 2.4;
-      vertical-align: middle;
-    }
-    input {
-      width: 100%;
-      height: 2.5em;
-      padding: 0 1em;
-      vertical-align: middle;
-      background: #fff;
-      border: 1px solid #bbb;
-      border-radius: 0 4px 4px 0;
-      line-height: 2.5;
-      &:hover {
-        border-color: #999;
+    &.single {
+      display: block;
+      > span {
+        display: block;
       }
-      &:focus {
-        border-color: #09f;
+      input {
+        display: block;
+        width: 100%;
+        padding: 0 .2em;
+        border-bottom: 1px solid #ddd;
+        font-size: .14rem;
+        line-height: 2;
+        color: #555;
+        transition: all .34s;
+        &:focus {
+          border-color: #50bfff;
+          color: #333;
+        }
+      }
+    }
+    &.base {
+      display: inline-table;
+      > * {
+        display: table-cell;
+      }
+      .label {
+        width: 1px;
+        height: 2.5em;
+        padding: 0 .1rem;
+        border-radius: 4px 0 0 4px;
+        border: 1px solid #bbb;
+        border-right: 0;
+        line-height: 2.4;
+        vertical-align: middle;
+      }
+      input {
+        width: 100%;
+        height: 2.5em;
+        padding: 0 1em;
+        vertical-align: middle;
+        background: #fff;
+        border: 1px solid #bbb;
+        border-radius: 0 4px 4px 0;
+        line-height: 2.5;
+        &:hover {
+          border-color: #999;
+        }
+        &:focus {
+          border-color: #09f;
+        }
       }
     }
   }
